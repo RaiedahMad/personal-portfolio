9c5c5733b84e2a781ec66d9651170c8970f28f91
Raied Ahmad <raiedahmad3@gmail.com>
2025-03-14 12:52:14 -0700

    second part about section done

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ window.addEventListener("mousemove", (e) => {
     ease: "power2.out",
   });
 });
+
+gsap.to(".letter", {
+  opacity: 1,
+  x: 0, // Side se aaye
+  y: () => gsap.utils.random(0, 0), // Har letter alag height se aaye
+  rotation: () => gsap.utils.random(0, 0), // Random rotation har letter ke liye
+  // rotationX: () => gsap.utils.random(-100, 60),
+  // rotationY: () => gsap.utils.random(-40, -60),
+  duration: 1,
+  stagger: 0.1, // Har letter delay ke sath aaye
+  ease: "power4.out",
+});
+
+gsap.to(".name1", {
+  x: 0,
+  scrollTrigger: {
+    trigger: ".name1",
+  },
+});
